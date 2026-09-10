b46f04348bfd78e7b3b2229b2529b553f109d985
Francis Bawa <francis_bawa@dlsu.edu.ph>
2026-09-10 23:07:26 +0800

    fix: switch CARTO tiles to keyed endpoint

--- src/features/home/CityMap.jsx
+++ src/features/home/CityMap.jsx
@@ -186,8 +186,10 @@ export default function CityMap({ selectedCity, onCitySelect }) {
         style={{ minHeight: isFullscreen ? "100vh" : "400px", background: "#f8f9fa" }}
       >
         <TileLayer
-          attribution=""
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url={`https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}{r}.png?key=${process.env.NEXT_PUBLIC_CARTO_KEY || ""}`}
+          subdomains="abcd"
+          maxZoom={20}
         />
         <FitBounds />
         <InvalidateOnResize isFullscreen={isFullscreen} />
